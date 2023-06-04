7d585589b942ea4a09c0d26a88ac91b816cd9ef2
Young Yang <young.yang03@sap.com>
2023-06-05 01:08:36 +0800

    modified:   goreleaser.sh

--- goreleaser.sh
+++ goreleaser.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git tag --delete v.1.2.229
-git push https://github.com/young-yang03/cloud-mta-build-tool.git --delete v.1.2.229
+git tag --delete v1.2.229
+git push https://github.com/young-yang03/cloud-mta-build-tool.git --delete v1.2.229
 
-git tag v.1.2.229
-git push https://github.com/young-yang03/cloud-mta-build-tool.git v.1.2.229
+git tag v1.2.229
+git push https://github.com/young-yang03/cloud-mta-build-tool.git v1.2.229
