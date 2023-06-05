1a4ecc3a3340a5fd8d13e22e30f3b439799058a6
Young Yang <young.yang03@sap.com>
2023-06-05 20:04:52 +0800

    modified:   .circleci/config.yml 	modified:   .goreleaser.yml 	modified:   Dockerfile_mbtci_template 	modified:   VERSION 	modified:   configs/version.yaml 	modified:   goreleaser.sh 	modified:   package.json

--- goreleaser.sh
+++ goreleaser.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git tag --delete v1.2.229
-git push https://github.com/young-yang03/cloud-mta-build-tool.git --delete v1.2.229
+git tag --delete v1.2.228
+git push https://github.com/young-yang03/cloud-mta-build-tool.git --delete v1.2.228
 
-git tag v1.2.229
-git push https://github.com/young-yang03/cloud-mta-build-tool.git v1.2.229
+git tag v1.2.228
+git push https://github.com/young-yang03/cloud-mta-build-tool.git v1.2.228
