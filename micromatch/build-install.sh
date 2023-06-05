af077f4b3fa38b191a214027bcdc7c22149913f8
Young Yang <young.yang03@sap.com>
2023-06-05 10:46:44 +0800

    modified:   micromatch-wrapper-Darwin 	modified:   micromatch-wrapper-Linux 	modified:   micromatch-wrapper-Windows 	modified:   micromatch/build-install.sh 	deleted:    micromatch/micromatch-wrapper-darwin 	deleted:    micromatch/micromatch-wrapper-linux 	deleted:    micromatch/micromatch-wrapper-windows

--- micromatch/build-install.sh
+++ micromatch/build-install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# build and install micromatch wrapper
+# clean env
 rm -rf ./micromatch-wrapper-linux ./micromatch-wrapper-macos ./micromatch-wrapper-win.exe
+rm -rf node_modules
 
+# build micromatch wrapper
 npm install
-
 pkg ./
 
+# install and test micromatch wrapper
 cp ./micromatch-wrapper-win.exe $GOPATH/bin/micromatch-wrapper.exe
-
 micromatch-wrapper.exe -h
+
+# clean and copy micromatch wrapper to target path for release to GitHub
+rm -rf ../micromatch-wrapper-Linux ../micromatch-wrapper-Darwin ../micromatch-wrapper-Windows
+mv ./micromatch-wrapper-linux ../micromatch-wrapper-Linux
+mv ./micromatch-wrapper-macos ../micromatch-wrapper-Darwin
+mv ./micromatch-wrapper-win.exe ../micromatch-wrapper-Windows
