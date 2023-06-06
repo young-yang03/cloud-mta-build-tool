5a2271a62f1d28a1c6d158e72d821315da08b9ee
Young Yang <young.yang03@sap.com>
2023-06-06 12:24:39 +0800

    deleted:    micromatch-wrapper-Darwin 	deleted:    micromatch-wrapper-Linux 	deleted:    micromatch-wrapper-Windows 	modified:   micromatch/Darwin/micromatch-wrapper 	modified:   micromatch/Linux/micromatch-wrapper 	modified:   micromatch/Windows/micromatch-wrapper 	modified:   micromatch/build-install.sh 	deleted:    micromatch/goreleaser-post-build.sh

--- micromatch/build-install.sh
+++ micromatch/build-install.sh
@@ -5,19 +5,19 @@ rm -rf ./micromatch-wrapper-linux ./micromatch-wrapper-macos ./micromatch-wrappe
 rm -rf node_modules
 
 # install pkg
-npm install pkg
-npx pkg --version
+npm install -g pkg
+pkg --version
 
 # build micromatch wrapper
 npm install
-npx pkg ./
+pkg ./
 
 # install and test micromatch wrapper
 cp ./micromatch-wrapper-win.exe $GOPATH/bin/micromatch-wrapper.exe
 micromatch-wrapper.exe -h
 
 # clean and copy micromatch wrapper to target path for release to GitHub
-rm -rf ../micromatch-wrapper-Linux ../micromatch-wrapper-Darwin ../micromatch-wrapper-Windows
-mv ./micromatch-wrapper-linux ../micromatch-wrapper-Linux
-mv ./micromatch-wrapper-macos ../micromatch-wrapper-Darwin
-mv ./micromatch-wrapper-win.exe ../micromatch-wrapper-Windows
+rm -rf ./Linux/* ./Darwin/* ./Windows/*
+mv ./micromatch-wrapper-linux ./Linux/micromatch-wrapper
+mv ./micromatch-wrapper-macos ./Darwin/micromatch-wrapper
+mv ./micromatch-wrapper-win.exe ./Windows/micromatch-wrapper
--- micromatch/goreleaser-post-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp ./micromatch/micromatch-wrapper-linux ./dist/cloud-mta-build-tool_darwin_amd64_v1
