31d242ac0649b870a2a9bd506a5981b11b56b00e
Young Yang <young.yang03@sap.com>
2024-07-04 16:18:02 +0800

    modified:   .circleci/config.yml 	modified:   internal/commands/commands.go

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -347,7 +347,7 @@ func GetModuleSBomGenCommands(loc *dir.Loc, module *mta.Module,
 		// cmd = "npm install " + cyclonedx_npm + "@" + cyclonedx_npm_version + " --no-save"
 		// cmds = append(cmds, cmd)
 		// cmd = "npx cyclonedx-npm --output-format " + strings.ToUpper(sbomFileType) + " --spec-version " + cyclonedx_npm_schema_version + " --output-file " + sbomFileName + sbomFileSuffix
-		cmd = "cyclonedx-npm --output-format " + strings.ToUpper(sbomFileType) + " --spec-version " + cyclonedx_npm_schema_version + " --output-file " + sbomFileName + sbomFileSuffix
+		cmd = "npx " + cyclonedx_npm + "@" + cyclonedx_npm_version + " --output-format " + strings.ToUpper(sbomFileType) + " --spec-version " + cyclonedx_npm_schema_version + " --output-file " + sbomFileName + sbomFileSuffix
 		cmds = append(cmds, cmd)
 	case "golang":
 		cmd = "cyclonedx-gomod mod -output-version 1.4 -licenses -output " + sbomFileName + sbomFileSuffix
